163ead50fd5927d5e88dbc9500e3b16a0e661abb
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-02-21 02:31:11 -0500

    Add project files.

---
+++ AlifMidTermProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlifMidTermProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
