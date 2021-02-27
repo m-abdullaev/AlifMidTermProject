b0b84d6cd73cb75d84e82c253ddfd7af04905b6f
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-02-27 07:55:15 -0500

    Created two methods: insert Admin select all Admin

---
+++ AlifMidTermProject/AdminCredentials.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AlifMidTermProject
+{
+    public static class AdminCredentials
+    {
+        public static string conString = @"Data source = DESKTOP-SS5TGJO\SQLEXPRESS; initial catalog = AlifMidTermProjectDB; Integrated security = true;";
+
+        public static int Id { get; set; }
+        public static string Login { get; set; }
+        public static string Password { get; set; }
+        public static SqlConnection connection = new SqlConnection(conString);
+        public static SqlCommand command = connection.CreateCommand();
+        public static void insertAdmin()
+        {
+            try
+            {
+                connection.Open();
+
+                //if (connection.State == System.Data.ConnectionState.Open)
+                //{
+                //    Console.WriteLine("Succesfully Connected to Data Base");
+                //}
+
+                Console.WriteLine("Please enter login");
+                string Login = Console.ReadLine();
+                Console.WriteLine("Please enter password");
+                string Password = Console.ReadLine();
+                command.CommandText = "insert into AdminCredentials(" +
+                    "Login," +
+                    "Password ) Values(" +
+                    $"'{Login}'," +
+                    $"'{Password}')";
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+
+                    Console.WriteLine("New admin successfully added!");
+
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public static void selectAllAdmin()
+        {
+            try
+            {
+                connection.Open();
+                //if (connection.State == System.Data.ConnectionState.Open)
+                //{
+                //    Console.WriteLine("Succesfully Connected to Data Base");
+                //}
+                command.CommandText = $"select * from AdminCredentials";
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader["Id"]}, Login: {reader["Login"]}, Password: {reader["Password"]} ");
+                }
+            }
+            catch (System.Exception ex)
+            {                
+                    Console.WriteLine(ex.Message);                
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        
+
+        }
+
+            }
+        
+    
+
--- AlifMidTermProject/Program.cs
+++ AlifMidTermProject/Program.cs
@@ -6,7 +6,42 @@ namespace AlifMidTermProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            switch (ShowMainMenu())
+            {
+                case "1":
+                    {
+                        Console.WriteLine(@"Please choose action: 
+                                 1. Create new Admin
+                                 2. Select all
+                                 3. Delete by Id
+                                 ");
+                        int action = Convert.ToInt32(Console.ReadLine());
+                        switch (action)
+                        {
+                            case 1:
+                                AdminCredentials.insertAdmin();
+                                break;
+                            case 2:
+                                AdminCredentials.selectAllAdmin();
+                                break;
+
+                        }
+
+                        break;
+
+                    }
+
+            }
+        }
+        static string ShowMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine($"{new string('-', 5)}Welcome to our bank{new string('-', 5)}");
+            Console.Write("1. Admin\n" +
+                          "2. Client\n" +                          
+                          "3. Exit\n" +
+                          "Choice:");
+            return Console.ReadLine();
         }
     }
 }
